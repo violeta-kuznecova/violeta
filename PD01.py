cb5e3d002d3107150e5e57dc7d64b399a4a1da23
violeta-kuznecova <violetakuznevowa8@gmail.com>
2024-12-09 13:06:49 +0200

    Add files via upload

---
+++ PD01.py
@@ -0,0 +1,37 @@
+width = 10
+height = 5
+
+for i in range(5):
+   print('10')
+   
+   
+   
+size = 6
+
+for i in range(6):
+    print('6')
+    
+height = 5
+
+for i in range(1, height + 1):
+
+    print(' ' * (height - i) + '*' * (2 * i - 1))
+     
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+    
+
+    
+    
